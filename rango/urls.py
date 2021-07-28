185e140d7f8edc68c3c0df7dc7c1ba7e1a962303
QiWang75 <2548066w@student.gla.ac.uk>
2021-07-29 01:44:18 +0800

    Chapter 3 exercises completed

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,5 @@ app_name = 'rango'
 urlpatterns = [
     # create path(string to match, view called if matched, reference of view by naming) function
     path('', views.index, name='index'), #call url and point to index view for mapping
+    path('about/', views.about, name='about'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -7,4 +7,8 @@ from django.http import HttpResponse
 # create index view
 # each view has at least 1 argument and must return httpresponse object
 def index(request):
-    return HttpResponse("Rango says hey there parter!")
+    return HttpResponse("Rango says hey there partner! <a href='/rango/about/'>About</a>")
+
+# create about view
+def about(request):
+    return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
